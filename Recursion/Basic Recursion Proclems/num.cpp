39941382731c86d856c152af9c70db8d3b763430
SHIVA099 <yadavshivansh2003@gmail.com>
2025-08-19 22:46:57 +0530

    dsa

--- Recursion/Basic Recursion Proclems/num.cpp
+++ Recursion/Basic Recursion Proclems/num.cpp
@@ -1,16 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int cnt = 0;
-void count(int n){
-    if(cnt == n) return;
-    cnt++;
-    cout << cnt << endl;
-    count(n);
+void num(int i, int n){
+    if(i>n) return;
+    cout << i << endl;
+    num(i+1, n);  
 }
 
 int main(){
     int n;
     cin >> n;
-    count(n);
+    num(1, n);
 }
+
+// ------------------------------------ METHOD 2 --------------------------------
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int cnt = 0;
+// void count(int n){
+//     if(cnt == n) return;
+//     cnt++;
+//     cout << cnt << endl;
+//     count(n);
+// }
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     count(n);
+// }
--- Recursion/Basic Recursion Proclems/revNum.cpp
+++ Recursion/Basic Recursion Proclems/revNum.cpp
@@ -1,16 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int cnt = 0;
-void count(int n){
-    if(cnt == 1) return;
-    cnt--;
-    cout << cnt << endl;
-    count(n);
+void revNum(int i, int n){
+    if(i<1) return;
+    cout << i << endl;
+    revNum(i-1, n);
 }
 
 int main(){
     int n;
-    cin >> cnt;
-    count(n);
+    cin >> n;
+    revNum(n, n);
 }
+
+// ------------------------------------ METHOD 2 --------------------------------
+
+// int cnt = 0;
+// void count(int n){
+//     if(cnt == 1) return;
+//     cnt--;
+//     cout << cnt << endl;
+//     count(n);
+// }
+
+// int main(){
+//     int n;
+//     cin >> cnt;
+//     count(n);
+// }
